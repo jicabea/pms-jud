2a0e5530ee0bdd43ca205964e2e261027c692986
Jicabea <beatricejika12@gmail.com>
2022-11-12 10:38:04 +0300

    PMS forms

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { DataTablesModule } from 'angular-datatables';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToolbarService, LinkService, ImageService, HtmlEditorService, TableService } from '@syncfusion/ej2-angular-richtexteditor';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    DataTablesModule,
+  ],
+  providers: [ToolbarService, LinkService, ImageService, HtmlEditorService, TableService ],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
